b32c599ac2a9153365b2dc89b1ce3b087f215862
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-10-23 19:20:07 +0545

    changes in _submit fn

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -17,6 +17,19 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
   final List<String> _priorities = ['Low', 'Medium', 'High'];
 
+  @override
+  void initState() { 
+    super.initState();
+    _dateController.text = _dateFormatter.format(_date);
+  }
+
+  @override
+  void dispose() { 
+    _dateController.dispose();
+    super.dispose();
+    
+  }
+
   _handleDatePicker() async{
     final DateTime date = await showDatePicker(
       context: context, 
@@ -37,6 +50,13 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
       print('$_title, $_date, $_priority');
+
+      //insert task to user's db
+
+      //update task
+
+
+      Navigator.pop(context);
     }
   }
 
@@ -72,7 +92,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                 key: _formKey,
                 child: Column(children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
                     child: TextFormField(
                       style: TextStyle(fontSize: 15.0),
                       decoration: InputDecoration(
@@ -89,7 +109,7 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                   ),
 
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
                     child: TextFormField(
                       readOnly: true,
                       controller: _dateController,
@@ -106,18 +126,19 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                   ),
 
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20.0),
+                    padding: EdgeInsets.symmetric(vertical: 15.0),
                     child: DropdownButtonFormField(
+                      isDense: true,
                       icon: Icon(Icons.arrow_drop_down_circle),
                       iconSize: 22.0,
                       iconEnabledColor: Theme.of(context).primaryColor,
                       items: _priorities.map((String priority) {
                         return DropdownMenuItem(
                           value: priority,
-                          child: Text(priority, style: TextStyle(color: Colors.black, fontSize: 18.0,),),
+                          child: Text(priority, style: TextStyle(color: Colors.black, fontSize: 15.0,),),
                         );
                       }).toList(),
-                      style: TextStyle(fontSize: 15.0),
+                      style: TextStyle(fontSize: 18.0),
                       decoration: InputDecoration(
                         labelText: 'Task Priority',
                         labelStyle: TextStyle(fontSize: 18.0),
