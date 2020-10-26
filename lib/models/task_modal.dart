85c64ae17124843da773c7ac80321e9296b7868c
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-10-26 20:02:22 +0545

    setting up task model 2

--- lib/models/task_modal.dart
+++ lib/models/task_modal.dart
@@ -12,12 +12,25 @@ class Task{
   Map<String, dynamic> toMap(){
     final map = Map<String, dynamic>();
 
-    map['id'] = id;
+    if (id != null) {
+      map['id'] = id;
+    }
+
     map['title'] = title;
     map['date'] = date.toIso8601String();
     map['priority'] = priority;
     map['status'] = status;
 
     return map;
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task.withId(
+      id: map['id'],
+      title: map['title'],
+      date: DateTime.parse(map['date']),
+      priority: map['priority'],
+      status: map['status'],
+    );
   }
 }
