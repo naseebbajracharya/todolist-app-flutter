408fa4b869ad7687e53a424c82b6f3238bf19bf7
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-11-04 14:15:09 +0545

    updt on helpers

--- lib/helpers/database_helper.dart
+++ lib/helpers/database_helper.dart
@@ -1,6 +1,15 @@
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
-  static final DatabaseHelper instance;
+  static final DatabaseHelper instance = DatabaseHelper._instance();
   static Database _db;
+
+  DatabaseHelper._instance();
+
+  String tasksTable = "task-table";
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDate = 'date';
+  String colPriority = 'priority';
+  String colStatus = 'status';
 }
