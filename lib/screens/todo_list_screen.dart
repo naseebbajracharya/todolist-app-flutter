692edc53ad757bfa2e3e9b9b35ceadb306c8b0f3
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-11-08 19:21:51 +0545

    setting up db configuration con. with ui 2

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -55,24 +55,29 @@ class _TodoListScreenState extends State<TodoListScreen> {
          ),
         ),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.symmetric(vertical: 40.0),
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          if(index == 0){
-            return Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('My Tasks', style: TextStyle(color: Colors.black, fontSize: 40.0, fontWeight: FontWeight.bold),),
-                SizedBox(height: 10.5,),
-                Text('1 of 5', style: TextStyle(color: Colors.grey, fontSize: 20.0, fontWeight: FontWeight.w600),),
-              ],
-              ),
-            );
-          }
-          return _buildTask(index);
-        }
+      body: FutureBuilder(
+        future: _taskList,
+        builder: (context, snapshot) {
+              return ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 40.0),
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            if(index == 0){
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('My Tasks', style: TextStyle(color: Colors.black, fontSize: 40.0, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 10.5,),
+                  Text('1 of 5', style: TextStyle(color: Colors.grey, fontSize: 20.0, fontWeight: FontWeight.w600),),
+                ],
+                ),
+              );
+            }
+            return _buildTask(index);
+          },
+        );
+        },
       ),
       
     );
