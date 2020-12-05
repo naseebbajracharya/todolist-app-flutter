663d0871898eb6a0324a3b26b28d155e44ede90e
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-12-05 10:01:36 +0545

    task list sorted

--- lib/helpers/database_helper.dart
+++ lib/helpers/database_helper.dart
@@ -50,6 +50,7 @@ class DatabaseHelper {
     taskMapList.forEach((taskMap) {
       taskList.add(Task.fromMap(taskMap));
     });
+    taskList.sort((taskA, taskB) => taskA.date.compareTo(taskB.date));
     return taskList;
   }
 
